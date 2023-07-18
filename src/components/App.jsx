ea44aaf7e7bd8e324ccaa0a9b2f55be88e35f154
Andrii Kruk <119102553+chodgi71@users.noreply.github.com>
2023-07-18 18:47:46 +0200

    routing + base structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,29 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import HomePage from 'pages/HomePage/HomePage';
+import MoviesPage from 'pages/MoviesPage/MoviesPage';
+
+import Layout from './Layout/Layout';
+import Movie from 'pages/Movie/Movie';
+import Reviews from './Reviews/Reviews';
+import Cast from './Cast/Cast';
+
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="movies" element={<MoviesPage />} />
+          <Route path="movie" element={<Movie />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+        </Route>
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
