854262f67b7f53425e864eb33dad026aaae3f88f
Andrii Kruk <119102553+chodgi71@users.noreply.github.com>
2023-07-22 16:57:08 +0200

    fix basename

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
   return (
     <div>
       <Suspense fallback={<div>Loading...</div>}>
-        <Routes basename="/goit-react-hw-05-movies">
+        <Routes >
           <Route path="/" element={<Layout />}>
             <Route index element={<HomePage />} />
             <Route path="movies" element={<MoviesPage />} />
